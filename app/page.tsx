23c8ac1ca8b935a309af0a8de4322521ce5886e0
Hasini Punsala <hasinipunsala@gmail.com>
2023-08-12 21:02:56 +0530

    fix the fetch projects error

--- app/page.tsx
+++ app/page.tsx
@@ -5,58 +5,62 @@ import { fetchAllProjects } from "@/lib/actions";
 
 type ProjectSearch = {
   projectSearch: {
-    edges: {node: ProjectInterface}[];
-    pageInfo:{
+    edges: { node: ProjectInterface }[];
+    pageInfo: {
       hasPreviousPage: boolean;
       hasNextPage: boolean;
       startCursor: string;
       endCursor: string;
-    }
-  }
-}
+    };
+  };
+};
 
 type SearchParams = {
-  category?: string;
-}
+  category?: string | null;
+};
 
 type Props = {
   searchParams: SearchParams;
-}
+};
 
-const Home = async ({searchParams: {category}}: Props) => {
+const Home = async ({ searchParams: { category } }: Props) => {
   const data = await fetchAllProjects(category) as ProjectSearch;
 
   const projectsToDisplay = data?.projectSearch?.edges || [];
 
-  if(projectsToDisplay.length === 0){
-    return(
+  if (projectsToDisplay.length === 0) {
+    return (
       <section className="flexStart flex-col paddings">
-        <Categories/>
+        <Categories />
 
-        <p className="no-result-text text-center">No projects found, go create some first.</p>
+        <p className="no-result-text text-center">
+          No projects found, go create some first.
+        </p>
       </section>
-    )
-  } 
+    );
+  }
 
   return (
     <section className="flex-start flex-col paddings mb-16">
-        <Categories/>
-        <section className="projects-grid">
-          {projectsToDisplay.map(({node}:{node: ProjectInterface}) => (
-            <ProjectCard
-              key={node?.id}
-              id={node?.id}
-              title={node?.title}
-              image={node?.image}
-              name={node?.createdBy?.name}
-              avatarUrl={node?.createdBy?.avatarUrl}
-              userId={node?.createdBy?.id}
-            />
-          ))}
-        </section>
-        <h1>LoadMore</h1>
+      <Categories />
+
+      <section className="projects-grid">
+        {projectsToDisplay.map(({ node }: { node: ProjectInterface }) => (
+          <ProjectCard
+            key={`${node?.id}`}
+            id={node?.id}
+            image={node?.image}
+            title={node?.title}
+            name={node?.createdBy.name}
+            avatarUrl={node?.createdBy.avatarUrl}
+            userId={node?.createdBy.id}
+          />
+        ))}
+      </section>
+
+      <h1>LoadMore</h1>
     </section>
   );
-}
+};
 
 export default Home;
